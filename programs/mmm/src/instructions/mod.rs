bc48f8c69b34e4760befd76265286827c4c0d059
Rex <rex@magiceden.io>
2022-10-13 13:35:44 -1000

    fmt

--- programs/mmm/src/instructions/mod.rs
+++ programs/mmm/src/instructions/mod.rs
@@ -1,21 +1,21 @@
 #![allow(missing_docs)]
 
-pub mod sol_close_pool;
 pub mod create_pool;
-pub mod sol_deposit_buy;
 pub mod deposit_sell;
+pub mod sol_close_pool;
+pub mod sol_deposit_buy;
 pub mod sol_fulfill_buy;
 pub mod sol_fulfill_sell;
-pub mod update_pool;
 pub mod sol_withdraw_buy;
+pub mod update_pool;
 pub mod withdraw_sell;
 
-pub use sol_close_pool::*;
 pub use create_pool::*;
-pub use sol_deposit_buy::*;
 pub use deposit_sell::*;
+pub use sol_close_pool::*;
+pub use sol_deposit_buy::*;
 pub use sol_fulfill_buy::*;
 pub use sol_fulfill_sell::*;
-pub use update_pool::*;
 pub use sol_withdraw_buy::*;
+pub use update_pool::*;
 pub use withdraw_sell::*;
