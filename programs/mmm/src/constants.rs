29cabf4839508ed7ecb31821772cf26c63d881c0
Rex <rex@magiceden.io>
2022-10-28 11:26:14 -0700

    Change to lower max for referral bp

--- programs/mmm/src/constants.rs
+++ programs/mmm/src/constants.rs
@@ -3,5 +3,5 @@ pub const POOL_PREFIX: &str = "mmm_pool";
 pub const SELL_STATE_PREFIX: &str = "mmm_sell_state";
 
 pub const MAX_METADATA_CREATOR_ROYALTY_BP: u16 = 3000;
-pub const MAX_REFERRAL_FEE_BP: u16 = 3000;
+pub const MAX_REFERRAL_FEE_BP: u16 = 500;
 pub const ALLOWLIST_MAX_LEN: usize = 6;
