67e79fd27873dcb0fc2928aeb41a44b280089b2e
madergaser <madergaser@users.noreply.github.com>
2023-07-17 17:06:59 -0500

    make struct fields public (#69)

--- programs/mmm/src/instructions/mip1/sol_mip1_fulfill_sell.rs
+++ programs/mmm/src/instructions/mip1/sol_mip1_fulfill_sell.rs
@@ -27,11 +27,11 @@ use crate::{
 
 #[derive(AnchorSerialize, AnchorDeserialize)]
 pub struct SolMip1FulfillSellArgs {
-    asset_amount: u64,
-    max_payment_amount: u64,
-    allowlist_aux: Option<String>, // TODO: use it for future allowlist_aux
-    maker_fee_bp: i16,             // will be checked by cosigner
-    taker_fee_bp: i16,             // will be checked by cosigner
+    pub asset_amount: u64,
+    pub max_payment_amount: u64,
+    pub allowlist_aux: Option<String>, // TODO: use it for future allowlist_aux
+    pub maker_fee_bp: i16,             // will be checked by cosigner
+    pub taker_fee_bp: i16,             // will be checked by cosigner
 }
 
 // FulfillSell means a buyer wants to buy NFT/SFT from the pool
