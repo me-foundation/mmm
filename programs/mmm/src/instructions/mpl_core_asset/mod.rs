17a993b0e3b06c584ca060b34ea0d7a0e96d4316
JeremyLi28 <jeremyliisagoodman@gmail.com>
2024-05-08 13:45:59 -0700

    format

--- programs/mmm/src/instructions/mpl_core_asset/mod.rs
+++ programs/mmm/src/instructions/mpl_core_asset/mod.rs
@@ -1,13 +1,13 @@
 #![allow(missing_docs)]
 
 pub mod mpl_core_deposit_sell;
-pub mod mpl_core_withdraw_sell;
-pub mod mpl_core_fulfill_sell;
 pub mod mpl_core_fulfill_buy;
+pub mod mpl_core_fulfill_sell;
+pub mod mpl_core_withdraw_sell;
 pub mod mpl_core_wrap;
 
 pub use mpl_core_deposit_sell::*;
-pub use mpl_core_withdraw_sell::*;
-pub use mpl_core_fulfill_sell::*;
 pub use mpl_core_fulfill_buy::*;
+pub use mpl_core_fulfill_sell::*;
+pub use mpl_core_withdraw_sell::*;
 pub use mpl_core_wrap::*;
