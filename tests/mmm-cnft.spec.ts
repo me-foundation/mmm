79df2f41ed7e3ad175a258b6039d48cda7880101
JeremyLi28 <jeremyliisagoodman@gmail.com>
2024-10-23 15:22:13 -0700

    basic cnft test set up

--- tests/mmm-cnft.spec.ts
+++ tests/mmm-cnft.spec.ts
@@ -1,9 +1,48 @@
 import * as anchor from '@project-serum/anchor';
+import { generateSigner, sol, Umi } from '@metaplex-foundation/umi';
+import {
+  createUmi,
+  getPubKey,
+  setupTreeAndListing,
+  verifyOwnership,
+} from './utils';
 
 describe('cnft tests', () => {
   const endpoint = 'http://localhost:8899';
   const conn = new anchor.web3.Connection(endpoint, 'processed');
+  let umi: Umi;
+
+  beforeAll(async () => {
+    umi = await createUmi(endpoint, sol(3));
+  });
+
   it.only('cnft fulfill buy', async () => {
-    console.log("cnft fulfill buy!!!!!!!!");
+    const umi = await createUmi(endpoint, sol(3));
+    const seller = generateSigner(umi);
+    await umi.rpc.airdrop(seller.publicKey, sol(1));
+    await umi.rpc.airdrop(seller.publicKey, sol(10));
+
+    const {
+      merkleTree,
+      escrowedProof,
+      leafIndex,
+      metadata,
+      getBubblegumTreeRef,
+      getCnftRef,
+    } = await setupTreeAndListing(umi, seller);
+
+    const merkleyTreePubkey = getPubKey(merkleTree);
+
+    console.log('merkleyTreePubkey', merkleyTreePubkey.toBase58());
+
+    // Verify that buyer now owns the cNFT.
+    await verifyOwnership(
+      umi,
+      merkleTree,
+      seller.publicKey,
+      leafIndex,
+      metadata,
+      [],
+    );
   });
 });
